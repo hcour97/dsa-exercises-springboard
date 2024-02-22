6c80c04f055676a17d65f152f1a526e2025ef939
Hannah <hcour@gmail.com>
2024-02-22 18:00:18 -0500

    further study dsa-sorting Qs

--- dsa-sorting/quick.js
+++ dsa-sorting/quick.js
@@ -3,12 +3,41 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, startIdx = 0, endIdx = arr.length - 1) {
+    const pivotValue = arr[startIdx];
+    let pivotIdx = startIdx
+    
+    for (let i = startIdx + 1; i < arr.length; i++) {
+        if (arr[i] < pivotValue) {
+            pivotIdx++;
+
+        // swap positions (next to each other)
+        [arr[i], arr[pivotIdx]] = [arr[pivotIdx], arr[i]];
+        }
+
+    }
+    // the pivot is still at arr[startIdx]
+    // so swap the value at the start index with the value at the pivot index:
+    [arr[startIdx], arr[pivotIdx]] = [arr[pivotIdx], arr[startIdx]];
+
+    return pivotIdx;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, leftIdx = 0, rightIdx = arr.length - 1) {
+    if (leftIdx < rightIdx) {
+        // partition array left and right
+        const pivotIdx = pivot(arr, leftIdx, rightIdx)
 
-module.exports = quickSort;
+        // call quicksort on the left and right pieces
+        // this will keep sorting them until there is nothing left to sort
+        quickSort(arr, leftIdx, pivotIdx - 1);
+        quickSort(arr, pivotIdx + 1, rightIdx);
+    }
+    return arr;
+}
+
+module.exports = {pivot, quickSort};
--- dsa-sorting/selection.js
+++ dsa-sorting/selection.js
@@ -1,3 +1,28 @@
-function selectionSort() {}
+/** selectionSort()
+ * 
+ * pseudocode:
+ * - Assign the first element to be the smallest value (it's ok if it's actually not) 
+ * - compare this to the next item, until you find a smaller number.
+ * - if a smaller number is found, reassign it as the new minimum and continue until end of the array.
+ * - if the "minimum" is not the value you started with, swap the value and now the beginning of the array is in the correct order. 
+ * - repeat until array is sorted.
+ * 
+ * not ideal, because quadratic runtime
+ * */
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        } 
+        // swap
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    } 
+    return arr;
+}
 
 module.exports = selectionSort;
